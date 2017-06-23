ecb2d44e0a066788aba0e72e5e08708ea15819f2
Steve <chengquan.wang@ele.me>
2017-06-23 18:07:45 +0800

    initial

---
+++ AppModule/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  AppModule
+//
+//  Created by Steve on 2017/6/23.
+//  Copyright © 2017年 Jack. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        view.backgroundColor = UIColor.red
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
